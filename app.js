bc60c6e839a22eb8658ca3b33389925f1169cd35
sossw1 <sossw1@gmail.com>
2021-12-02 11:49:27 -0500

    created 'add' command

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const yargs = require('yargs');
 
 yargs.version('1.0.0');
 
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!');
+  }
+});
