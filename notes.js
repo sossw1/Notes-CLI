b11240023676fcae56b93028b3e3ede9470d3af5
sossw1 <sossw1@gmail.com>
2021-12-03 10:14:35 -0500

    saveNotes converts to JSON string and writes to file

--- notes.js
+++ notes.js
@@ -7,6 +7,12 @@ function addNote(title, body) {
     body: body
   });
   console.log('Adding note: ', { title, body });
+  saveNotes(notes);
+}
+
+function saveNotes(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
 }
 
 function loadNotes() {
